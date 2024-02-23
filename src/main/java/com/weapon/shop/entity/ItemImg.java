f3103e912179c98f28c08f91039c035733d3121d
kyerim1 <nilzero25@gmail.com>
2024-02-23 17:30:51 +0900

    shop 1일

---
+++ src/main/java/com/weapon/shop/entity/ItemImg.java
@@ -0,0 +1,35 @@
+package com.weapon.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+public class ItemImg {
+    @Id
+    @Column(name="item_img_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private String imgName; //이미지 이름
+    @Column
+    private String oriImgName; // 원본 이미지 이름
+    @Column
+    private String imgUrl; // 이미지 경로
+    @Column
+    private String repImgYn; // 대표이미지 설정
+
+    @Column
+    private LocalDateTime regDate;
+    @Column
+    private LocalDateTime updateDate;
+
+    @ManyToOne
+    @JoinColumn(name="item_id")
+    private Item item;
+
+}
